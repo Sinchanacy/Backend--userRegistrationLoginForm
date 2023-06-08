80baf0cd1672cfd55691f787f226ea6cb57f6e3d
Sinchana C Y <1ms19ee056@gmail.com>
2023-06-08 15:00:24 +0530

    second commit

--- src/test/java/com/apiautomation/apitesting/POSTAPIRequest.java
+++ src/test/java/com/apiautomation/apitesting/POSTAPIRequest.java
@@ -2,6 +2,7 @@ package com.apiautomation.apitesting;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,7 @@ public class POSTAPIRequest  {
     public void postRequestForRegistrationWithValidEmail() throws JSONException {
         JSONObject object=new JSONObject();
         object.put("firstname","testuser3");
-        object.put("email","testuser3@gmail.com");
+        object.put("email","testuser5@gmail.com");
         object.put("password","testuser");
         RestAssured.given()
                 .contentType(ContentType.JSON)
@@ -23,7 +24,6 @@ public class POSTAPIRequest  {
                 then()
                 .assertThat().statusCode(200);
     }
-
 
     @Test
     public void postRequestForRegistrationWithExistingEmail() throws JSONException {
@@ -40,4 +40,135 @@ public class POSTAPIRequest  {
                 then()
                 .assertThat().statusCode(403);
     }
+
+    @Test
+    public void postRequestForUserAuthenticationForValidEmailPassword() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("email","testuser@gmail.com");
+        object.put("password","testuser");
+        Response responset=
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(object.toString())
+                .baseUri("http://localhost:8080/welcome/authenticate")
+                .when()
+                .post()
+                .then()
+                .assertThat().statusCode(200)
+                .extract().response();
+        System.out.println("Response=>" + responset.getBody().asString());
+    }
+    @Test
+    public void postRequestForUserAuthenticationForNotVerifiedEmailPassword() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("email","testuser3@gmail.com");
+        object.put("password","testuser");
+       RestAssured.given()
+                        .contentType(ContentType.JSON)
+                        .body(object.toString())
+                        .baseUri("http://localhost:8080/welcome/authenticate")
+                        .when()
+                        .post()
+                        .then()
+                         .assertThat().statusCode(403);
+
+
+    }
+    @Test
+    public void postRequestForUserAuthenticationForValidEmailInvalidPassword() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("email","testuser@gmail.com");
+        object.put("password","testuser1");
+                RestAssured.given()
+                        .contentType(ContentType.JSON)
+                        .body(object.toString())
+                        .baseUri("http://localhost:8080/welcome/authenticate")
+                        .when()
+                        .post()
+                        .then()
+                        .assertThat().statusCode(403);
+    }
+    @Test
+    public void postRequestForUserAuthenticationForInvalidEmailPassword() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("email","testuser8gmail.com");
+        object.put("password","testuser");
+        Response response=
+                RestAssured.given()
+                        .contentType(ContentType.JSON)
+                        .body(object.toString())
+                        .baseUri("http://localhost:8080/welcome/authenticate")
+                        .when()
+                        .post()
+                        .then()
+                        .assertThat().statusCode(200)
+                        .extract().response();
+        System.out.println("Response=>" + response.getBody().asString());
+    }
+
+    @Test
+    public void postRequestForForgotPasswordForValidEmail() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("email","testuser@gmail.com");
+        Response response=
+                RestAssured.given()
+                        .contentType(ContentType.JSON)
+                        .body(object.toString())
+                        .baseUri("http://localhost:8080/resetpassword")
+                        .when()
+                        .post()
+                        .then()
+                        .assertThat().statusCode(200)
+                        .extract().response();
+        System.out.println("Response=>" + response.getBody().asString());
+    }
+    @Test
+    public void postRequestForForgotPasswordForInvalidEmail() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("email","testuser9@gmail.com");
+        RestAssured.given()
+                        .contentType(ContentType.JSON)
+                        .body(object.toString())
+                        .baseUri("http://localhost:8080/resetpassword")
+                        .when()
+                        .post()
+                        .then()
+                        .assertThat().statusCode(403);
+    }
+    @Test
+    public void postRequestForSavePasswordWithValidPasswordResetToken() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("newPassword","testusers");
+        Response response=
+                RestAssured.given()
+                        .contentType(ContentType.JSON)
+                        .body(object.toString())
+                        .queryParam("token","9eea027d-3a27-4686-97a1-8de414987a09")
+                        .baseUri("http://localhost:8080/savePassword")
+                        .when()
+                        .post()
+                        .then()
+                        .assertThat().statusCode(200)
+                        .extract().response();
+        System.out.println("Response => " + response.getBody().asString());
+    }
+    @Test
+    public void postRequestForSavePasswordWithInValidPasswordResetToken() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("newPassword","testusers");
+        Response response=
+                RestAssured.given()
+                        .contentType(ContentType.JSON)
+                        .body(object.toString())
+                        .queryParam("token","9ea027d-3a27-4686-97a1-8de414987a09")
+                        .baseUri("http://localhost:8080/savePassword")
+                        .when()
+                        .post()
+                        .then()
+                        .assertThat().statusCode(200)
+                        .extract().response();
+        System.out.println("Response => " + response.getBody().asString());
+    }
+
 }
+ 
